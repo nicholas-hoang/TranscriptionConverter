290fa2fffb370623fe8ed0d5778da6d30b9c66ca
Nicholas Hoang <68435507+nvh232@users.noreply.github.com>
2023-10-06 15:25:46 -0500

    uploads

--- converter.py
+++ converter.py
@@ -2,8 +2,20 @@ import pandas as pd
 import numpy as np
 from docx import Document
 from docx.shared import Inches
+import main_gui
+
 
 def docx_to_txt(file):
+    """
+    Converts a Word document to a DataFrame.
+
+    Args:
+        file (str): The path to the Word document.
+
+    Returns:
+        pandas.DataFrame: The Word document converted to a DataFrame.
+
+    """
     document = Document(file)
     doc = []
     for para in document.paragraphs:
@@ -13,23 +25,47 @@ def docx_to_txt(file):
 
 
 def format_table(dataframe):
+    """
+    Formats the input DataFrame into a table with columns for the timestamp, speaker, and text.
+
+    Args:
+        df (pandas.DataFrame): The input DataFrame containing the transcript.
+
+    Returns:
+        pandas.DataFrame: The input DataFrame formatted into a table with columns for the timestamp, speaker, and text.
+
+    """
     df = dataframe
     # Split the transcripts into a list of strings
-    df = df[0].str.split('\n')
+    df = df[0].str.split("\n")
     df = df.apply(pd.Series)
     # Rename the columns
-    df.rename(columns={0:'TimeStamp', 1:'Speaker',2:'Text'}, inplace=True)
+    df.rename(columns={0: "TimeStamp", 1: "Speaker", 2: "Text"}, inplace=True)
     # Split the timestamp into start and end times
-    df.TimeStamp = df['TimeStamp'].apply(lambda x: x.split(' --> '))
-    df['Start'] = df['TimeStamp'].apply(lambda x: x[0])
-    df['End'] = df['TimeStamp'].apply(lambda x: x[1])
+    df.TimeStamp = df["TimeStamp"].apply(lambda x: x.split(" --> "))
+    df["Start"] = df["TimeStamp"].apply(lambda x: x[0])
+    df["End"] = df["TimeStamp"].apply(lambda x: x[1])
 
     return df
 
+
 def map_speakers(dataframe):
+    """
+    Maps each speaker to a unique integer label.
+
+    Args:
+        df (pandas.DataFrame): The input DataFrame containing the speaker column.
+
+    Returns:
+        pandas.DataFrame: The input DataFrame with a new column containing the integer labels for each speaker.
+        dict: A dictionary mapping each speaker to a unique integer label.
+
+    """
     df = dataframe
-    speakers_dict = {speaker: index for index, speaker in enumerate(df.Speaker.unique())}
-    df['Label'] = df.Speaker.map(speakers_dict)
+    speakers_dict = {
+        speaker: index for index, speaker in enumerate(df.Speaker.unique())
+    }
+    df["Label"] = df.Speaker.map(speakers_dict)
     return df, speakers_dict
 
 
@@ -46,64 +82,101 @@ def map_speakers(dataframe):
 #     speaker_to_redact = input('Which speaker would you like to redact? ')
 
 
-def concatenate_text_with_timestamp_and_speaker_by_label(df):
+def concatenate_text_with_timestamp_and_speaker_by_label(dataframe):
+    """
+    Concatenates the text, start timestamp, end timestamp, and speaker name for each group of rows with the same label
+    in the input DataFrame.
+
+    Args:
+        df (pandas.DataFrame): The input DataFrame containing the text, timestamp, and speaker columns.
+
+    Returns:
+        list: A list of dictionaries, where each dictionary contains the concatenated text, start timestamp, end timestamp,
+        and speaker name for a group of rows with the same label.
+    """
+    df = dataframe
     concatenated_data = []
     current_text = ""
     start_timestamp = None
     speaker = None
 
     for index, row in df.iterrows():
-        if index == 0 or row['Label'] == df.at[index - 1, 'Label']:
+        if index == 0 or row["Label"] == df.at[index - 1, "Label"]:
             if not start_timestamp:
-                start_timestamp = row['TimeStamp'][0]
+                start_timestamp = row["TimeStamp"][0]
             if not speaker:
-                speaker = row['Speaker']
-            current_text += " " + row['Text']
-            end_timestamp = row['TimeStamp'][1]
+                speaker = row["Speaker"]
+            current_text += " " + row["Text"]
+            end_timestamp = row["TimeStamp"][1]
         else:
-            concatenated_data.append({'text': current_text.strip(), 'start_timestamp': start_timestamp, 'end_timestamp': end_timestamp, 'speaker': speaker})
-            current_text = row['Text']
-            start_timestamp = row['TimeStamp'][0]
-            end_timestamp = row['TimeStamp'][1]
-            speaker = row['Speaker']
+            concatenated_data.append(
+                {
+                    "text": current_text.strip(),
+                    "start_timestamp": start_timestamp,
+                    "end_timestamp": end_timestamp,
+                    "speaker": speaker,
+                }
+            )
+            current_text = row["Text"]
+            start_timestamp = row["TimeStamp"][0]
+            end_timestamp = row["TimeStamp"][1]
+            speaker = row["Speaker"]
 
-    concatenated_data.append({'text': current_text.strip(), 'start_timestamp': start_timestamp, 'end_timestamp': end_timestamp, 'speaker': speaker})
+    concatenated_data.append(
+        {
+            "text": current_text.strip(),
+            "start_timestamp": start_timestamp,
+            "end_timestamp": end_timestamp,
+            "speaker": speaker,
+        }
+    )
     return concatenated_data
 
 
 def write_to_word_doc(concatenated_data, filename):
+    """
+    Writes the concatenated text, start timestamp, end timestamp, and speaker name for each group of rows to a Word document.
+
+    Args:
+        concatenated_data (list): A list of dictionaries, where each dictionary contains the concatenated text, start timestamp,
+        end timestamp, and speaker name for a group of rows with the same label.
+        filename (str): The name of the file to save the Word document as.
+
+    Returns:
+        None
+    """
     # Create a new Word document
     document = Document()
 
     # Add a heading to the document
-    document.add_heading('Meeting Transcription', level=0)
+    document.add_heading("Meeting Transcription", level=0)
 
     # Loop through the concatenated data and add each group to the document
     for group in concatenated_data:
         # Add the start and end timestamps to the document
-        document.add_paragraph(f"[{group['start_timestamp']} - {group['end_timestamp']}]")
+        document.add_paragraph(
+            f"[{group['start_timestamp']} - {group['end_timestamp']}]"
+        )
 
         # Add the speaker name to the document and make it bold.
         speaker_paragraph = document.add_paragraph()
         speaker_paragraph.add_run(f"Speaker: {group['speaker']}").bold = True
 
         # Add the concatenated text to the document
-        document.add_paragraph(group['text'])
+        document.add_paragraph(group["text"])
 
     # Save the document
     document.save(filename)
 
 
-def main():
-    file = '/Users/nicholashoang/Documents/GitHub/TranscriptionConverter/data/Transcript Cleaning Automation Discussion_2023-10-02[62].docx'
-    df = docx_to_txt(file)
+def main(file_path):
+    df = docx_to_txt(file_path)
     df = format_table(df)
     df, speakers_dictionary = map_speakers(df)
     # redact_speakers_input(df, speakers_dictionary)
     concatenate_data = concatenate_text_with_timestamp_and_speaker_by_label(df)
-    write_to_word_doc(concatenate_data, 'data/concatenated_text.docx')
+    write_to_word_doc(concatenate_data, "data/concatenated_text.docx")
 
 
-if __name__ == '__main__':
-    main()
-
+if __name__ == "__main__":
+    pass
---
+++ main_gui.py
@@ -0,0 +1,169 @@
+import tkinter as tk
+from tkinter import filedialog
+import os
+from docx import Document
+import pandas as pd
+import numpy as np
+
+
+class TranscriptionConverter:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def docx_to_txt(self):
+        document = Document(self.file_path)
+        doc = [para.text for para in document.paragraphs]
+        df = pd.DataFrame(doc, columns=["Text"])
+        return df
+
+    def format_table(self, dataframe):
+        # Split the transcripts into a list of strings
+        df = dataframe["Text"].str.split("\n", expand=True)
+        # Rename the columns
+        df.columns = ["TimeStamp", "Speaker", "Text"]
+        # Split the timestamp into start and end times
+        df[["Start", "End"]] = df["TimeStamp"].str.split(" --> ", expand=True)
+        return df
+
+    def map_speakers(self, dataframe):
+        speakers_dict = {
+            speaker: index
+            for index, speaker in enumerate(dataframe["Speaker"].unique())
+        }
+        dataframe["Label"] = dataframe["Speaker"].map(speakers_dict)
+        return dataframe, speakers_dict
+
+    def concatenate_text_with_timestamp_and_speaker_by_label(self, dataframe):
+        concatenated_data = []
+        current_text = ""
+        start_timestamp = None
+        speaker = None
+
+        for index, row in dataframe.iterrows():
+            if (
+                start_timestamp is None
+                or row["Label"] == dataframe.at[index - 1, "Label"]
+            ):
+                if start_timestamp is None:
+                    start_timestamp = row["Start"]
+                if speaker is None:
+                    speaker = row["Speaker"]
+                current_text += " " + row["Text"]
+                end_timestamp = row["End"]
+            else:
+                concatenated_data.append(
+                    {
+                        "text": current_text.strip(),
+                        "start_timestamp": start_timestamp,
+                        "end_timestamp": end_timestamp,
+                        "speaker": speaker,
+                    }
+                )
+                current_text = row["Text"]
+                start_timestamp = row["Start"]
+                end_timestamp = row["End"]
+                speaker = row["Speaker"]
+
+        concatenated_data.append(
+            {
+                "text": current_text.strip(),
+                "start_timestamp": start_timestamp,
+                "end_timestamp": end_timestamp,
+                "speaker": speaker,
+            }
+        )
+        return concatenated_data
+
+    def write_to_word_doc(self, concatenated_data, filename):
+        document = Document()
+        document.add_heading("Meeting Transcription", level=0)
+
+        for group in concatenated_data:
+            document.add_paragraph(
+                f"[{group['start_timestamp']} - {group['end_timestamp']}]"
+            )
+            speaker_paragraph = document.add_paragraph()
+            speaker_paragraph.add_run(f"Speaker: {group['speaker']}").bold = True
+            document.add_paragraph(group["text"])
+
+        document.save(filename)
+
+    def convert_and_write(self, output_filename="data/concatenated_text-CLEANED.docx"):
+        df = self.docx_to_txt()
+        df = self.format_table(df)
+        df, speakers_dictionary = self.map_speakers(df)
+        concatenated_data = self.concatenate_text_with_timestamp_and_speaker_by_label(
+            df
+        )
+        self.write_to_word_doc(concatenated_data, output_filename)
+
+
+class FileUploader(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Audio Transcription Formatter for Rapid Analysis by. Nick Hoang")
+        self.geometry("600x400")
+        self.setup_gui()
+
+    def setup_gui(self):
+        # Left Frame for File Selection
+        left_frame = tk.Frame(self)
+        left_frame.pack(side="left", fill="both", padx=20, pady=20)
+
+        # File Selection Label and Entry
+        select_label = tk.Label(
+            left_frame, text="Select a File:", font=("Helvetica", 16)
+        )
+        select_label.grid(row=0, column=0, sticky="w", pady=(0, 10))
+
+        self.file_path_entry = tk.Entry(left_frame, font=("Helvetica", 12), width=60)
+        self.file_path_entry.grid(row=0, column=1, padx=(10, 0), pady=(0, 10))
+
+        # Buttons (centered on the same row)
+        select_button = tk.Button(
+            left_frame, text="Browse", command=self.select_file, font=("Helvetica", 14)
+        )
+        select_button.grid(row=1, column=0, pady=(0, 10), sticky="e")
+
+        self.run_button = tk.Button(
+            left_frame,
+            text="Run",
+            font=("Helvetica", 14),
+            state="disabled",
+            command=self.run_module,
+        )
+        self.run_button.grid(row=1, column=1, padx=(10, 0), pady=(0, 10), sticky="w")
+
+        # Feedback Label
+        self.feedback_label = tk.Label(
+            left_frame, text="", font=("Helvetica", 12), fg="green"
+        )
+        self.feedback_label.grid(row=3, column=0, columnspan=2)
+
+        #
+
+    def select_file(self):
+        file_path = filedialog.askopenfilename()
+        if file_path:
+            self.file_path_entry.delete(0, tk.END)  # Clear previous text
+            self.file_path_entry.insert(0, file_path)  # Insert selected file path
+            self.run_button.config(state="active")  # Enable the run button
+
+    def run_module(self):
+        file_path = self.file_path_entry.get()  # Get the selected file path
+        if file_path and os.path.exists(file_path):
+            converter = TranscriptionConverter(file_path)
+            converter.convert_and_write()
+            self.feedback_label.config(
+                text="File processing completed successfully. ヾ( ˃ᴗ˂ )◞ • *✰ ",
+                fg="green",
+            )
+        else:
+            self.feedback_label.config(
+                text="Error: Invalid file path or file does not exist. (¬_¬')", fg="red"
+            )
+
+
+if __name__ == "__main__":
+    app = FileUploader()
+    app.mainloop()
